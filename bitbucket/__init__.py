e29293f185e279d85950cfb7dc26155e4abf0385
Rafal Janicki <rafal@kult.io>
2018-08-16 15:40:07 +0200

    Bum up version

--- bitbucket/__init__.py
+++ bitbucket/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.5.0'
+__version__ = '1.0.0'
 
 __doc__ = """
 Bitbucket has a REST API publicly available, this package provide methods to interact with it.
